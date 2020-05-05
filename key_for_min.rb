dd662dc179576614feeec67599382e51abfaf643
Luke Chen <luke.chen.2010@gmail.com>
2020-05-05 19:41:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,9 @@ def key_for_min_value(name_hash)
   min_name = nil
   
   name_hash.each do |key, value|
-    min = value if min == nil
-    
-    if value < min
+    if min == nil
+       min = value
+    elsif value < min
       min = value 
       min_name = key
     end
